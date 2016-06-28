5f5af57a745f99b9ceaf37217be3e45b92894eba
bob rubbens <bobrubbens@gmail.com>
2016-06-28 10:55:49 +0200

    Removed all files that were tracked but also in gitignore.

--- test6006/test6006.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef TEST6006_H
-#define TEST6006_H
-#include <vector>
-std::vector<int> test6006(std::vector<int> xs);
-#endif
--- test6007/test6007.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef TEST6007_H
-#define TEST6007_H
-#include <vector>
-std::vector<int> test6007(std::vector<int> xs, std::vector<int> ys);
-#endif
